b758172a2a4cc206cc9cc63c94be27580d90fafd
Piotr Bątor <pbator94@gmail.com>
2021-04-14 18:51:07 +0200

    Add content to subpages

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const contactRouter = require("./routes/contact");
 const servicesRouter = require("./routes/services");
 const specialOffersRouter = require("./routes/specialOffers");
 const expressesRouter = require("./routes/expresses");
-
+  
 var app = express();
 
 // view engine setup
@@ -19,7 +19,7 @@ app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
